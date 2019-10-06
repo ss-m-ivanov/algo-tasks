f042feaef00640d4d79531493d10ee01681d5f61
ss-m-ivanov <maks.ivanov.work@gmail.com>
2019-10-06 18:25:40 +0300

    Fixed bug in 107 task

--- algo_tasks.py
+++ algo_tasks.py
@@ -4,7 +4,7 @@ from math import sqrt
 def task_num_107():
     """ Дано целое число m > 1 Получить наибольшое целое к, при котором 4^к < m """
     m = int(input('\'Task 107\' Input your natural number : '))
-    counter = 0
+    counter = 1
     a = 4
     while a < m:
         a *= a
